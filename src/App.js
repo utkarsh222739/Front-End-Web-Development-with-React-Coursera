af9791b0dcf2b3b6c28b47efe2f58351810e0923
utkarsh222739 <utkarsh222739@gmail.com>
2021-04-29 21:39:49 +0530

    Header and Footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, {Component} from 'react';
 import './App.css';
-import Menu from './components/MenuComponent.js';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import { DISHES } from './shared/dishes';
 import Main from './components/MainComponent';
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.css';
+import 'bootstrap-social/bootstrap-social.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
